aaeb26c74ff3aa4129cefe3e3e27b66422017f58
mkeilman <mvkeilman+github@gmail.com>
2024-07-23 09:27:18 -0600

    vite build

--- heads-up/vite.config.js
+++ heads-up/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/",
-    build: {
-        rollupOptions: {
-            nested: resolve(__dirname, 'index.html'),
-        },
-    },
+//    build: {
+//        rollupOptions: {
+//            nested: resolve(__dirname, 'index.html'),
+//        },
+//    },
     plugins: [react()],
 })
