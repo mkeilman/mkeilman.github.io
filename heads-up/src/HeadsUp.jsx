0d45680ff416e4c72b2cf9709cd50670b6b4f591
mkeilman <mvkeilman+github@gmail.com>
2024-07-26 09:49:44 -0600

    starting headsup

---
+++ heads-up/src/HeadsUp.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './css/App.css'
+
+const HeadsUp = () => {
+
+  return (
+    <>
+      <h1>Heads Up!</h1>
+      <div>
+          bets
+      </div>
+      <div>
+          board
+      </div>
+    </>
+  )
+};
+
+export default HeadsUp
