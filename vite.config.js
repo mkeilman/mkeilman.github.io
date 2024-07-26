b1753d6e43941d665dc2cbfb69ec94d0b4191b35
mkeilman <mvkeilman+github@gmail.com>
2024-07-26 09:07:22 -0600

    moving to multipage

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/",
-//    build: {
-//        rollupOptions: {
-//            nested: resolve(__dirname, 'index.html'),
-//        },
-//    },
+    build: {
+        rollupOptions: {
+            main: resolve(__dirname, 'index.html'),
+            headsUp: resolve(__dirname, 'heads-up/index.html'),
+        },
+    },
     plugins: [react()],
 })
