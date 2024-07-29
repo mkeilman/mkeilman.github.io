2b6b13dfbdc685c18177395ab01c84e4b851445f
mkeilman <mvkeilman+github@gmail.com>
2024-07-29 10:05:22 -0600

    utils

---
+++ heads-up/src/js/Utils.js
@@ -0,0 +1,15 @@
+class Utils {
+    	static randomIndicesForArrayOfSize(size) {
+            const arr= [];
+            const iArr = new Array(size).fill(0);
+            let temp = 0;
+            for (let i = 0; i < size; ++i) {
+                const r =  Math.floor(Math.random() * (size - i));
+                arr.push(iArr[r]);
+                temp = iArr[r];
+                iArr[r] = iArr[size - i - 1];
+                iArr[size - i - 1] = temp;
+            }
+            return arr;
+	    }
+}
