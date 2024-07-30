9ff292a5a96d81a6ebb7d3822dae6a713c9ebdc6
mkeilman <mvkeilman+github@gmail.com>
2024-07-30 11:20:58 -0600

    ckp

--- heads-up/src/js/Utils.js
+++ heads-up/src/js/Utils.js
@@ -1,29 +1,29 @@
 class Utils {
-        static indexArray(size) {
-            const arr = new Array(size);
-            for (let i = 0; i < size - 1; ++i) {
-                arr[i] = i;
-            }
-            return arr;
+    static indexArray(size) {
+        const arr = new Array(size);
+        for (let i = 0; i < size - 1; ++i) {
+            arr[i] = i;
         }
+        return arr;
+    }
 
-        static log(...items) {
-            console.log(items);
+    static log(...items) {
+        console.log(items);
+    }
+
+    static randomIndicesForArrayOfSize(size) {
+        const arr= [];
+        const iArr = Utils.indexArray(size);
+        let temp = 0;
+        for (let i = 0; i < size; ++i) {
+            const r =  Math.floor(Math.random() * (size - i));
+            arr.push(iArr[r]);
+            temp = iArr[r];
+            iArr[r] = iArr[size - i - 1];
+            iArr[size - i - 1] = temp;
         }
-
-        static randomIndicesForArrayOfSize(size) {
-            const arr= [];
-            const iArr = Utils.indexArray(size);
-            let temp = 0;
-            for (let i = 0; i < size; ++i) {
-                const r =  Math.floor(Math.random() * (size - i));
-                arr.push(iArr[r]);
-                temp = iArr[r];
-                iArr[r] = iArr[size - i - 1];
-                iArr[size - i - 1] = temp;
-            }
-            return arr;
-	    }
+        return arr;
+    }
 }
 
 export {Utils}
