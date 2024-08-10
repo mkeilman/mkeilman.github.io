0dc6b87f31681df3ce06488e3054bb6289fcf6b2
mkeilman <mvkeilman+github@gmail.com>
2024-08-09 20:42:12 -0600

    added tests

---
+++ heads-up/tests/utils.test.js
@@ -0,0 +1,26 @@
+import { expect, test } from 'vitest'
+import {Utils} from '../src/js/Utils.js';
+
+test('capitalize abc to Abc', () => {
+    expect(Utils.capitalize('abc')).toBe('Abc');
+});
+
+
+test('pick elements', () => {
+    const arrToPick = [2, 4, 6, 8, 10];
+    const posArr  = [0, 1, 4];
+    const pickedArr = Utils.elementsAtPositions(arrToPick, posArr);
+    expect(pickedArr).toStrictEqual([2, 4, 10]);
+});
+
+
+/*
+
+
+	func testJSON() {
+		let gm = GameModel(numPlayers: 2)
+		let json = gm.toJSON()
+
+		XCTAssertTrue(JSONSerialization.isValidJSONObject(json))
+
+ */
