1f19b8f3b43f3a48101c14f82e786f72996a7762
mkeilman <mvkeilman+github@gmail.com>
2024-08-15 08:51:24 -0600

    pot tests

--- heads-up/src/js/BoundMethodsObject.js
+++ heads-up/src/js/BoundMethodsObject.js
@@ -8,6 +8,22 @@ class BoundMethodsObject {
                 this[x] = this[x].bind(this);
             });
     }
+
+    funcs() {
+        return Object.getOwnPropertyNames(this).filter(x => typeof this[x] === 'function');
+    }
+
+    objs() {
+        return this.props().filter(x => typeof this[x] === 'object');
+    }
+
+    props() {
+        return Object.getOwnPropertyNames(this).filter(x => ! this.funcs().includes(x));
+    }
+
+    vals() {
+        return this.props().filter(x => typeof this[x] !== 'object');
+    }
 }
 
 export {BoundMethodsObject}
--- heads-up/tests/pokerPot.test.js
+++ heads-up/tests/pokerPot.test.js
@@ -1,15 +1,13 @@
 import { expect, test } from 'vitest'
 import {PokerPlayer} from '../src/js/PokerPlayer.js';
+import {PokerPot} from '../src/js/PokerPot.js';
 import {Utils, debugLog} from '../src/js/Utils.js';
 
 test('json', () => {
-    const p = new PokerPlayer();
-    p.pokerFace = PokerPlayer.PokerFaces.cool;
-    const pp = PokerPlayer.fromJSON(p.toJSON());
+    const p = new PokerPot(200);
+    p.addPlayer(new PokerPlayer());
+    const pp = PokerPot.fromJSON(p.toJSON());
     p.vals().forEach(x => {
         expect(p[x]).toBe(pp[x]);
-    });
-    p.objs().forEach(x => {
-        expect(p[x]).toStrictEqual(pp[x]);
     });
 });
