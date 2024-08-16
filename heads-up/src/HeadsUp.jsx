f4c467fa0723289687c82b25092ed6352b251b6c
mkeilman <mvkeilman+github@gmail.com>
2024-08-16 10:46:03 -0600

    ckp

--- heads-up/src/HeadsUp.jsx
+++ heads-up/src/HeadsUp.jsx
@@ -11,21 +11,10 @@ import {Utils, debugLog} from './js/Utils.js';
 import Board from './Board.jsx';
 
 const HeadsUp = () => {
-    //let [deck, setDeck] = useState(() => new PokerDeck());
-    //let [players, setPlayers] = useState(() => [new PokerPlayer(), new PokerPlayer()]);
-    //let [game, setGame] = useState(() => new PokerGame(players.length));
     let [game, setGame] = useState(() => new PokerGame(2));
-
     const mgr = new GameManager(game);
 
-    let playerIndex = 0;
-    //const numPlayers = players.length;
-    //game.players = players;
-
     const deal = () => {
-        //const c = deck.dealCard();
-        //players[playerIndex].currentCards.push(c);
-        //playerIndex = (playerIndex + 1) % numPlayers;
         mgr.nextDeal();
         setGame(PokerGame.fromJSON(mgr.game.toJSON()));
     };
@@ -40,10 +29,8 @@ const HeadsUp = () => {
     const shuffle = () => {
         mgr.game.deck.shuffle();
         setGame(PokerGame.fromJSON(mgr.game.toJSON()));
-        //setDeck(new PokerDeck(true));
     };
-    //<Deck deck={mgr.game.deck}></Deck>
-    //<button onClick={shuffle}>Shuffle</button>
+    
     return (
         <>
             <h1>Heads Up!</h1>
