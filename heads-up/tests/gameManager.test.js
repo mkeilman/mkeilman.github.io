dc1212b9876cc9cb0f620ae67b11f35253ecd310
mkeilman <mvkeilman+github@gmail.com>
2024-08-22 13:51:47 -0600

    mgr tests

---
+++ heads-up/tests/gameManager.test.js
@@ -0,0 +1,12 @@
+import { expect, test } from 'vitest';
+import {GameManager} from '../src/js/GameManager.js';
+import {PokerGame} from '../src/js/PokerGame.js';
+import {PokerPlayer} from '../src/js/PokerPlayer.js';
+import {Utils, debugLog} from '../src/js/Utils.js';
+
+test('bets', () => {
+    //const mgr = new GameManager();
+    //let action = GameManager.BetActions.minBet;
+    //let bet = mgr.determineBet(action);
+    //debugLog('BET', action, bet);
+});
