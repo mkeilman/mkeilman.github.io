65a9e0caf62379b3160e5257488e68ae195a6924
mkeilman <mvkeilman+github@gmail.com>
2025-11-14 09:49:41 -0700

    gift exchange react

---
+++ keilman-gift-exchange/src/GiftExchange.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './css/HeadsUp.css'
+
+
+const GiftExchange = () => {
+
+    return (
+        <div>
+            Code<input type="text"></input>
+            <button>Enter</button>
+        </div>
+    )
+};
+
+export default GiftExchange;
