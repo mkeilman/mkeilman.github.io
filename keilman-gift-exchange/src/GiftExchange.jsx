e8d7dffcfa4c6e3554ba0ba898f7a14725447f6c
mkeilman <mvkeilman+github@gmail.com>
2025-11-16 20:08:39 -0700

    ckp

--- keilman-gift-exchange/src/GiftExchange.jsx
+++ keilman-gift-exchange/src/GiftExchange.jsx
@@ -4,22 +4,33 @@ import './css/GiftExchange.css'
 let exchangeData;
 
 window.onload = async () => {
-    const res = await fetch('https://mkeilman.github.io/keilman-gift-exchange/data/2025.json');
-    exchangeData = await res.json();
+    try {
+        const res = await fetch('https://mkeilman.github.io/keilman-gift-exchange/data/2025.json');
+        exchangeData = await res.json();
+    }
+    catch (error) {
+        console.log(error);
+    }
+
 }
 
 const GiftExchange = () => {
 
     const handleClick = (userCode) => {
         console.log(userCode);
-        console.log(exchangeData);
+        //console.log(exchangeData);
+        gifteeLabel = document.getElementById("gifteeLabel");
+        if (! exchangeData) {
+            gifteeLabel.
+        }
     };
 
 
     return (
         <div>
-            Code<input name="userCode" id="user_code" type="text"></input>
-            <button onClick={(e) => handleClick(document.getElementById("user_code").value)}>Enter</button>
+            <label>Code</label><input id="userCode" type="text"></input>
+            <button onClick={(e) => handleClick(document.getElementById("userCode").value)}>Enter</button>
+            <div id="gifteeLabel"></div>
         </div>
     )
 };
